e0447eff6af5f62ba1301e78f7b791b7b96df442
EderFabiano <98755595+EderFabiano@users.noreply.github.com>
2023-11-09 12:39:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 //Variáveis
 var nome="Abner";
-var QtdExp=100000;
+var qtdExp=100000;
 var nivel;
 
 //Estrutura de decisão
-if(QtdExp <=1000){
+if(qtdExp <=1000){
   nivel = "Ferro";
-}else if(QtdExp >1000 && QtdExp<=2000){
+}else if(qtdExp >1000 && qtdExp<=2000){
   nivel = "Bronze";
-}else if (QtdExp >2000 && QtdExp<=6000){
+}else if (qtdExp >2000 && qtdExp<=6000){
   nivel = "Prata";
-}else if (QtdExp >6000 && QtdExp<=7000){
+}else if (qtdExp >6000 && qtdExp<=7000){
   nivel="Ouro";
-}else if (QtdExp>7000 && QtdExp<=8000){
+}else if (qtdExp>7000 && qtdExp<=8000){
   nivel = "Platina";
-}else if (QtdExp >8000 && QtdExp <=9000){
+}else if (qtdExp >8000 && qtdExp <=9000){
   nivel="Ascendente";
-}else if(QtdExp >9000 && QtdExp<=10000){
+}else if(qtdExp >9000 && qtdExp<=10000){
   nivel="Imortal";
 }else{
   nivel="Radiante"; 
